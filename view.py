3031c60b4ddc850e9c3f98b53df045d22966552e
Alexander <kir-al93@yandex.ru>
2022-04-27 18:48:22 +0900

    Refactored app.py

---
+++ view.py
@@ -0,0 +1,28 @@
+from flask import request, render_template, send_from_directory
+
+from app import app
+
+
+@app.route("/")
+def page_index():
+    return "/"
+
+
+@app.route("/list")
+def page_tag():
+    pass
+
+
+@app.route("/post", methods=["GET", "POST"])
+def page_post_form():
+    pass
+
+
+@app.route("/post", methods=["POST"])
+def page_post_upload():
+    pass
+
+
+@app.route("/uploads/<path:path>")
+def static_dir(path):
+    return send_from_directory("uploads", path)
